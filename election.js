a877e124f37b201ac6db7682c980b55b649fbf7b
Courtney Noonan <courtneynoonan@me.com>
2016-09-08 14:32:54 -0400

    working on voting buttons

--- election.js
+++ election.js
@@ -1,37 +1,37 @@
 $(document).ready(function pollResults(){
 
-  $.ajax({
+  $.ajax({                                              // get JSON date for candidate and vote from heroku
     url: 'https://bb-election-api.herokuapp.com/',
     method: 'GET',
     data: { },
     dataType: 'JSON'
-  }).done(function(responseData){
-    var candidateJSON = responseData;
-    var candidateList = candidateJSON["candidates"];
-    $('#results').empty();
+  }).done(function(responseData) {
+      var candidateList = responseData["candidates"];
+      $('#results').empty();
 
-    for(i=0; i<candidateList.length; i++) {
-      var candidate = candidateList[i];
-      var listItem = document.createElement('li')
-      var voteButton = document.createElement('button');
+      for(i=0; i<candidateList.length; i++) {
+        var candidate = candidateList[i];
+        var listItem = document.createElement('li')         // create an <li> for each candidate with vote count
+        var voteButton = document.createElement('button');  // create a <button> to post votes
 
-      $(listItem).attr('data-name', candidate.name);
-      $(listItem).html(candidate.name + ': ' + candidate.votes);
-      $('#results').append(listItem);
-      $(listItem).append(voteButton);
-      $(voteButton).html('VOTE for ' + candidate.name);
-      $(voteButton).attr('id', candidate.name);
-    }
+        $(listItem).attr('data-name', candidate.name);
+        $(listItem).html(candidate.name + ': ' + candidate.votes);
+        $('#results').append(listItem);
+        $(listItem).append(voteButton);
+        $(voteButton).attr('id', candidate.name);
+        $(voteButton).html('VOTE for ' + candidate.name);
+      }
+      //PROBLEMS
+      $('#' + candidate.name).on('click', function(){  // only the <button> for gary responds to this
+        $.ajax({
+          url: 'https://bb-election-api.herokuapp.com/vote',
+          method: 'POST',
+          data: { "name" : candidate.name }, // this will post a vote but I should be able to use $(this).data('id')
+          dataType: 'HTML'
+        });                                 // I think that i should have the second ajax call outside of the first one,
+      });                                   // but I'm not sure how to access the responseData outside of it
 
-    $('button').on('click', function(){
-      $.ajax({
-        url: 'https://bb-election-api.herokuapp.com/vote',
-        method: 'POST',
-        data: $(this).data('name')
-      });
     });
-
-  });
 
   $('button').on('click', pollResults);
 
