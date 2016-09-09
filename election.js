2f64967937a56feb05289cfe967dcf77c117a958
Courtney Noonan <courtneynoonan@me.com>
2016-09-08 22:59:03 -0400

    completing voting buttons

--- election.js
+++ election.js
@@ -13,23 +13,24 @@ $(document).ready(function pollResults(){
         var candidate = candidateList[i];
         var listItem = document.createElement('li')         // create an <li> for each candidate with vote count
         var voteButton = document.createElement('button');  // create a <button> to post votes
+        $(voteButton).addClass('vote-button');
 
-        $(listItem).attr('data-name', candidate.name);
         $(listItem).html(candidate.name + ': ' + candidate.votes);
         $('#results').append(listItem);
         $(listItem).append(voteButton);
-        $(voteButton).attr('id', candidate.name);
+        $(voteButton).attr('data-id', candidate.id);
         $(voteButton).html('VOTE for ' + candidate.name);
+
       }
-      //PROBLEMS
-      $('#' + candidate.name).on('click', function(){  // only the <button> for gary responds to this
+
+      $('.vote-button').on('click', function(eventObject){  // only the <button> for gary responds to this
         $.ajax({
           url: 'https://bb-election-api.herokuapp.com/vote',
           method: 'POST',
-          data: { "name" : candidate.name }, // this will post a vote but I should be able to use $(this).data('id')
-          dataType: 'HTML'
-        });                                 // I think that i should have the second ajax call outside of the first one,
-      });                                   // but I'm not sure how to access the responseData outside of it
+          data: { "id" : $(this).data('id') },
+          dataType: 'json'
+        });
+      });
 
     });
 
